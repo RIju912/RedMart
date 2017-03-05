c5941da99dd6cbbb5d08b33ac6ba38ee34ebf103
Subhodip <differentialthinking@gmail.com>
2017-03-06 00:28:57 +0530

    Constants added!

---
+++ RedMart/Utility/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  RedMart
+//
+//  Created by Subhodip Banerjee on 06/03/17.
+//  Copyright © 2017 Subhodip. All rights reserved.
+//
+
+import Foundation
+
+struct UrlConstants {
+    
+    static var productListAPI = "https://api.redmart.com/v1.5.7/catalog/search?theme=all-sales&"
+    static var productImageAPI = "http://media.redmart.com/newmedia/200p"
+    
+}
+
